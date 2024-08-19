398c40506cd2f9dd22068a433e29f07133d0f678
Joy Pal <96969424+Joy-hazard@users.noreply.github.com>
2024-08-20 01:02:59 +0600

    Delete CENSUS Website directory

--- CENSUS Website/admin_logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["email"]);
-unset($_SESSION["name"]);
-header("Location: admin_login_page.php");
-?>
--- CENSUS Website/citizens_logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["email"]);
-unset($_SESSION["name"]);
-header("Location: Citizens_login_page.php");
-?>
